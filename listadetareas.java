e834a46ec85a605969ca2e6006f162c7044ddac3
mriamz <143622067+mriamz@users.noreply.github.com>
2023-09-06 08:38:44 -0600

    Add files via upload

---
+++ listadetareas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyec;
+
+/**
+ *
+ * @author Usuario
+ */
+public class listadetareas {
+    public static void main(String[] args) {
+        String lavar=" 1 lavar trasrtes ";
+        String barrer="2 barrer cocina ";
+        String trapear="3 trapear cocina";
+        String limpiar ="4 limpiar estufa";
+        String sacudir =" 5 sacudir muebles";
+        System.out.println("mi tarea es  " +lavar );
+          System.out.println("mi tarea es  " +barrer );
+          System.out.println("mi tarea es  " + trapear);
+          System.out.println("mi tarea es  " + limpiar);
+          
+          System.out.println("mi tarea es  " + sacudir);
+    }
+}
