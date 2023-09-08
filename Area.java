0935bfc9bd76650f2f53036a8d5a1e900abad529
mriamz <143622067+mriamz@users.noreply.github.com>
2023-09-08 08:54:34 -0600

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package area;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Area {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // TODO code application logic here
+        double a=10,b=5;
+        double area = a*b /2;
+        System.out.println("el area del tiriangulo es "+area);
+        System.out.println("");
+        
+    }
+    
+}
